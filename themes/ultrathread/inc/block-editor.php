dcad42ffeac3e4b26c8a3df2ecc40a88c20fe519
raoulex nurse <raoulexnurse@yahoo.com>
2022-05-07 22:14:19 -0400

    block-editor file updated with necessary code

--- themes/ultrathread/inc/block-editor.php
+++ themes/ultrathread/inc/block-editor.php
@@ -4,3 +4,12 @@
  *
  * @package UltraThreads
  */
+function ultrathreads_enqueue_block_editor_assets() {
+    wp_enqueue_script(
+        'block-editor-script',
+        get_template_directory_uri() . '/assets/js/block-editor.js',
+        array( 'wp-blocks', 'wp-dom-ready', 'wp-edit-post' ),
+        filemtime( get_template_directory_uri() . '/assets/js/block-editor.js' )
+    );
+}
+add_action( 'enqueue_block_editor_assets', 'ultrathreads_enqueue_block_editor_assets' );
