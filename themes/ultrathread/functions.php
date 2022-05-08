fa96e1a132a41b38cf0a15cff13fcceabff58a13
raoulex nurse <raoulexnurse@yahoo.com>
2022-05-08 12:18:15 -0400

    enqeue scrtipts and styles for foundations added to functions.php

--- themes/ultrathread/functions.php
+++ themes/ultrathread/functions.php
@@ -128,9 +128,35 @@ add_action( 'widgets_init', 'ultrathread_widgets_init' );
  * Enqueue scripts and styles.
  */
 function ultrathread_scripts() {
-	wp_enqueue_style( 'ultrathread-style', get_stylesheet_uri(), array(), ULTRATHREAD_VERSION );
+	wp_enqueue_style( 
+	'ultrathread-style', 
+	get_stylesheet_uri(), 
+	array(),
+	 ULTRATHREAD_VERSION
+	 );
 	
+	 wp_enqueue_style( 
+		'foundation-style', 
+		get_template_directory_uri() . '/assets/css/vendor/foundation.min.css', 
+		array(),
+		 '6.7.4'
+		 );
 
+	wp_enqueue_script( 
+			'what-input-script', 
+			get_template_directory_uri() . '/assets/js/vendor/what-input.js', 
+			array( 'jquery' ),
+			 '5.2.10',
+			 true
+			 );	 
+
+	wp_enqueue_script( 
+			'foundation-script', 
+			get_template_directory_uri() . '/assets/js/vendor/foundation.min.js', 
+			array( 'jquery', 'what-input-script' ),
+			 '6.7.4',
+			 true
+			 );	 
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
