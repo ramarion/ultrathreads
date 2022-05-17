1e9efc05913b8424af4e769bff0464bbc96cec34
raoulex nurse <raoulexnurse@yahoo.com>
2022-05-17 06:33:52 -0400

    woocommerce block editor added

--- themes/ultrathread/inc/woocommerce.php
+++ themes/ultrathread/inc/woocommerce.php
@@ -1,6 +1,13 @@
 <?php
 /**
- * Functions which enhance the theme by hooking into the woocommerce
+ * Functions which enhance the theme by hooking into the Woocommerce
  *
  * @package UltraThreads
  */
+function ultrathread_use_block_editor_for_post_type( $use_block_editor, $post_type) {
+    if ( 'product' === $post_type ) {
+        $use_block_editor = true;
+    }
+    return $use_block_editor;
+}
+ add_filter('use_block_editor_for_post_type','ultrathread_use_block_editor_for_post_type', 10, 2 );
