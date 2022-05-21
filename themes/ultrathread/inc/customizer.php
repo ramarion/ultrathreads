cbe3b72169bd164b3b2bf0064351835e9bca6e6a
raoulex nurse <raoulexnurse@yahoo.com>
2022-05-21 18:51:54 -0400

    color section,setting, and control added to customizerphp

--- themes/ultrathread/inc/customizer.php
+++ themes/ultrathread/inc/customizer.php
@@ -13,6 +13,7 @@
 function ultrathread_customize_register( $wp_customize ) {
 	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
 	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
 
 
 	if ( isset( $wp_customize->selective_refresh ) ) {
@@ -31,6 +32,49 @@ function ultrathread_customize_register( $wp_customize ) {
 			)
 		);
 	}
+	// Add New Section: Colors
+    $wp_customize->add_section( 'ultrathread_color_section', array(
+		'title' => 'Colors',
+		'description' => 'Set Colors For Background , Headers & Footers',
+		'priority' => '40'                  
+));
+
+// Add Settings: Color 
+$wp_customize->add_setting( 'ultrathread_theme_bgcolor', array(
+'default' => '#ffe4c4',    
+));
+
+$wp_customize->add_setting( 'ultrathread_header_bgcolor', array(
+'default' => '#003e1f',                        
+));
+
+$wp_customize->add_setting( 'ultrathread_footer_bgcolor', array(
+'default' => '#003e1f',                        
+));
+
+
+// Add Controls
+$wp_customize->add_control( new WP_Customize_Color_Control( $wp_customize, 'ultrathread_theme_bgcolor', array(
+'label' => 'Theme Background',
+'section' => 'ultrathread_color_section',
+'settings' => 'ultrathread_theme_bgcolor'
+
+)));
+
+
+$wp_customize->add_control( new WP_Customize_Color_Control( $wp_customize, 'ultrathread_header_bgcolor', array(
+'label' => 'Header Background',
+'section' => 'ultrathread_color_section',
+'settings' => 'ultrathread_header_bgcolor'
+)));
+
+$wp_customize->add_control( new WP_Customize_Color_Control( $wp_customize, 'ultrathread_footer_bgcolor', array(
+'label' => 'Footer Background',
+'section' => 'ultrathread_color_section',
+'settings' => 'ultrathread_footer_bgcolor'
+)));
+
+
 }
 add_action( 'customize_register', 'ultrathread_customize_register' );
 
