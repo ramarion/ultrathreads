9d643ebfb4491bbadea323e9fe888785086d52b2
raoulex nurse <raoulexnurse@yahoo.com>
2022-05-25 23:52:51 -0400

    files included in php

--- themes/ultrathread/functions.php
+++ themes/ultrathread/functions.php
@@ -207,9 +207,7 @@ function ultrathread_scripts() {
 			'6.1.1',
 			true
 			);	 
-	 
 
- 
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
