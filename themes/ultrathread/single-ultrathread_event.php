c9d2e55e816fdbc106619a8504e0a43ed55b243c
raoulex nurse <raoulexnurse@yahoo.com>
2022-05-29 01:21:50 -0400

    modified single post

--- themes/ultrathread/single-ultrathread_event.php
+++ themes/ultrathread/single-ultrathread_event.php
@@ -11,21 +11,15 @@ get_header();
 ?>
 
 	<main id="primary" class="site-main">
-
+    <div class="single-post">
 		<?php
 		while ( have_posts() ) :
 			the_post();
 
-			echo 'This is the single recipe template';
-				
+
 			get_template_part( 'template-parts/content', get_post_type() );
 
-			the_post_navigation(
-				array(
-					'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'ultrathread' ) . '</span> <span class="nav-title">%title</span>',
-					'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'ultrathread' ) . '</span> <span class="nav-title">%title</span>',
-				)
-			);
+			
 
 			// If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) :
@@ -34,7 +28,7 @@ get_header();
 
 		endwhile; // End of the loop.
 		?>
-
+	</div>
 	</main><!-- #main -->
 
 <?php
